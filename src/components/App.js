a7251d380158b4471e6d7747cadfbfeb0fdacb8e
Adrian Rodriguez <adrianwritenow@gmail.com>
2017-09-16 18:03:34 -0400

    finished , minimal styling

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import '../styles/App.css';
+
+import BookList from '../containers/BookList';
+import BookDetail from '../containers/BookDetail';
+
+class App extends Component {
+    render() {
+        return (
+          <div className= 'container'>
+            <h1>Books N Roses: Appetite for Instruction</h1>
+              <div className="row">
+                  <BookList/>
+                  <BookDetail/>
+              </div>
+          </div>
+        );
+    }
+}
+
+export default App;
